52abb02e979cd9449ee5f41090dd9ba2f0b4fa4c
cloozo <mybenefitchoiceinc@gmail.com>
2022-02-16 21:06:56 -0500

    app setup

---
+++ README.md
@@ -0,0 +1,37 @@
+
+## Jonacin Pierre-Louis - 2022 ||Book Search Engine 2022
+---
+---
+# User Story
+AS AN avid reader
+I WANT to search for new books to read
+SO THAT I can keep a list of books to purchase
+
+---
+
+
+## React Porfolio: 
+Link to my github repo-> (https://github.com/cloozo/Book-Search-Engine-2022/tree/master)
+## Direct Link to Deployed App: 
+Link to deployed App can be found here-> (https://fast-plains-24526.herokuapp.com)
+
+![https://github.com/cloozo/Book-Search-Engine-2022/tree/master](https://user-images.githubusercontent.com/84163696/153095489-a5b4669e-df79-477c-addd-72616b49b09c.png)
+
+---
+# Technologies Used
+- React
+- Node Js
+- Express Js
+- GraphQL
+- Mongoose
+- Mongo DB
+
+
+---
+---
+ Overall, it was a great learning experience and this activity has helped me greatly advanced in my road to becoming a web developper.
+# Assignment: Book Search Engine 2022
+
+By Jonacin Pierre-Louis, 2022
+Book Search Engine 2022
+
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+//
+
+// creating appolo server
+const { ApolloServer } = require("apollo-server-express");
+//authMiddleware
+const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./schemas");
+const app = express();
+//create port variables
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+server.applyMiddleware({ app });
+
+// ...serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+//
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+//
+db.once("open", () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
